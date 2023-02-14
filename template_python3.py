4cc1a147f115c57a1fc1c4d7564c1bdc97333cc0
j1schmid <j1schmid@users.noreply.github.com>
2023-02-14 16:39:20 +0100

    template_python3.py: replace old fashion argument handling with argparser

--- template_python3.py
+++ template_python3.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys
+import argparse
+
+parser = argparse.ArgumentParser(
+		prog = 'ProgramName',
+		description = 'What the program does',
+		epilog = 'Text at the bottom of help')
+
+parser.add_argument('filename')										# positional argument
+parser.add_argument('-c', '--count')								# option that takes a value
+parser.add_argument('-v', '--verbose', action='store_true') 		# on/off flag
 
 #import code # entering the interactive mode at a certain point: code.interact(local=dict(globals(), **locals())), Ctrl+D and the script goes on
 
 
-usage_str='''
-Usage: ./eagle_bom_to_ssp.py FILE
-.....
-.
-'''
-def main():
-    if len(sys.argv) != 2:
-		print('error: wrong number of arguments')
-		print(usage_str)
-		sys.exit()
-
+def main(cfg):
+	print(cfg)
+    
 
 if __name__ == '__main__':
-	main()
+	cfg = parser.parse_args()
+	main(cfg)
